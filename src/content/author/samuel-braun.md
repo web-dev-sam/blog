b5e8a2c0ad24f6a365e7fb24d42d31a6347a9f80
Samuel Braun <office.samigo.a@gmail.com>
2023-04-08 15:30:20 +0200

    Blog written

---
+++ src/content/author/samuel-braun.md
@@ -0,0 +1,8 @@
+---
+name: Samuel Braun
+image: /assets/author/pfp/samuel-braun.jpg
+devto: https://dev.to/samuel-braun
+linkedin: https://www.linkedin.com/in/samuel-braun/
+---
+
+Samuel Braun
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,29 @@
-// 1. Import utilities from `astro:content`
-
 import { z, defineCollection } from "astro:content";
 
-// 2. Define a schema for each collection you'd like to validate.
-const blogCollection = defineCollection({
-    schema: z.object({
-        draft: z.boolean().default(false),
-        date: z.date().transform((str) => new Date(str)),
-        title: z.string(),
-        tags: z.array(z.string()).optional(),
-        share: z
-            .object({
-                image: z.string().url().optional(),
-                title: z.string(),
-                description: z.string(),
-            })
-            .strict(),
-    }),
-});
-
-// 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-    blog: blogCollection,
+    blog: defineCollection({
+        schema: z.object({
+            draft: z.boolean().default(false),
+            date: z.date().transform((str) => new Date(str)),
+            title: z.string(),
+            author: z.string(),
+            cover: z.string().optional(),
+            tags: z.array(z.string()).optional(),
+            share: z
+                .object({
+                    image: z.string().url().optional(),
+                    title: z.string(),
+                    description: z.string(),
+                })
+                .strict(),
+        }),
+    }),
+    author: defineCollection({
+        schema: z.object({
+            name: z.string(),
+            image: z.string(),
+            devto: z.string().url().optional(),
+            linkedin: z.string().url().optional(),
+        }),
+    }),
 };
