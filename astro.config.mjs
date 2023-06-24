87cff3b307014894fb879c341e89b33361c30b91
Samuel Braun <office.samigo.a@gmail.com>
2023-06-24 15:31:14 +0200

    fixed sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.webry.com",
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), sitemap()]
 });
